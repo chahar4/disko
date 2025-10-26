e21239cff3d08c03dbdd00414d267a9086930b64
chahar4 <rezacharsetad@outlook.com>
2025-10-26 20:16:57 +0330

    clean channel:make model look more readable

--- internal/channel/model.go
+++ internal/channel/model.go
@@ -2,18 +2,13 @@ package channel
 
 import "context"
 
+// channel
 type Channel struct {
 	ID      int    `json:"id"`
 	Name    string `json:"name"`
 	GuildID int    `json:"guild_id"`
 }
 
-type Message struct {
-	ID         int    `json:"id"`
-	Channel_ID int    `json:"channel_id"`
-	Author_ID  int    `json:"author_id"`
-	Content    string `json:"content"`
-}
 type AddChannelReq struct {
 	Name    string `json:"name"`
 	GuildID int    `json:"guild_id"`
@@ -25,6 +20,27 @@ type AddChannelRes struct {
 	GuildID int    `json:"guild_id"`
 }
 
+// message
+type AddMessageReq struct {
+	Channel_ID int    `json:"channel_id"`
+	Author_ID  int    `json:"author_id"`
+	Content    string `json:"content"`
+}
+
+type Message struct {
+	ID         int    `json:"id"`
+	Channel_ID int    `json:"channel_id"`
+	Author_ID  int    `json:"author_id"`
+	Content    string `json:"content"`
+}
+type AddMessageRes struct {
+	ID         int    `json:"id"`
+	Channel_ID int    `json:"channel_id"`
+	Author_ID  int    `json:"author_id"`
+	Content    string `json:"content"`
+}
+
+// interfaces
 type Repository interface {
 	AddChannel(ctx context.Context, channel *Channel) (*Channel, error)
 	AddMessage(ctx context.Context, message *Message) (*Message, error)
@@ -33,17 +49,4 @@ type Repository interface {
 type Service interface {
 	AddGuild(ctx context.Context, req *AddChannelReq) (*AddChannelRes, error)
 	AddMessage(ctx context.Context, req *AddMessageReq) (*AddMessageRes, error)
-}
-
-type AddMessageReq struct {
-	Channel_ID int    `json:"channel_id"`
-	Author_ID  int    `json:"author_id"`
-	Content    string `json:"content"`
-}
-
-type AddMessageRes struct {
-	ID         int    `json:"id"`
-	Channel_ID int    `json:"channel_id"`
-	Author_ID  int    `json:"author_id"`
-	Content    string `json:"content"`
 }
